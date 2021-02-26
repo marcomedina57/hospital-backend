2633e91cd487354b95d26fb60891f4b118e06409
Marco Medina <marcos57@hotmail.com>
2021-02-26 13:41:34 -0700

    Spa control

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var cors = require('cors');
 const express = require('express');
 
 require('dotenv').config();
-
+const path = require('path');
 // Configurar CORS
 
 const app = express();
@@ -41,7 +41,13 @@ app.use(express.static('public'))
   app.use('/api/upload', require('./routes/upload'));
 
 
+  // lo ultimo
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+  })
+
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
-    
+
 });
